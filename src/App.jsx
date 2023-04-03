f0be8d6344b73ce7222e4dbd6fe4f1ffad2d42d2
AxrorjonA <adhamjonovaxrorjon@gmail.com>
2023-04-03 17:55:05 +0500

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from './components/Navbar'
-import Header from './components/Header/'
+import Header from './components/Card'
 import Portfolio from './components/Portfolio'
 function App() {
   return (
